44ba920e28ff3df775278cb2b938a99e2699a7f9
og5c5yy <og5c5yinyong@jichen.live>
2022-10-18 20:42:45 +0800

    填写页面

--- panel/index.js
+++ panel/index.js
@@ -1,21 +1,28 @@
+var fs = require('fs');
+
+const PACKAGE_NAME = 'ccc-load-points';
+
 // panel/index.js, this filename needs to match the one registered in package.json
 Editor.Panel.extend({
   // css style for panel
-  style: `
-    :host { margin: 5px; }
-    h2 { color: #f90; }
-  `,
+  // style: `
+  //   :host { margin: 5px; }
+  //   h2 { color: #f90; }
+  // `,
 
   // html template for panel
-  template: `
-    <h2>ccc-load-points</h2>
-    <hr />
-    <div>State: <span id="label">--</span></div>
-    <hr />
-    <ui-button id="btn">Send To Main</ui-button>
-    <hr />
-    <ui-button id="dump">dump points</ui-button>
-  `,
+  // template: `
+  //   <h2>ccc-load-points</h2>
+  //   <hr />
+  //   <div>State: <span id="label">--</span></div>
+  //   <hr />
+  //   <ui-button id="btn">Send To Main</ui-button>
+  //   <hr />
+  //   <ui-button id="dump">dump points</ui-button>
+  // `,
+  template: fs.readFileSync(Editor.url(`packages://${PACKAGE_NAME}/panel/index.html`), 'utf8'),
+
+  style: fs.readFileSync(Editor.url(`packages://${PACKAGE_NAME}/panel/index.css`), 'utf8'),
 
   // element and variable binding
   $: {
@@ -26,7 +33,7 @@ Editor.Panel.extend({
 
   // method executed when template and styles are successfully loaded and initialized
   ready () {
-    this.$btn.addEventListener('confirm', () => {
+    // this.$btn.addEventListener('confirm', () => {
       // Editor.Ipc.sendToMain('ccc-load-points:clicked');
       // Editor.Ipc.sendToMain('ccc-load-points:say-hello', 'Hello, this is a test message');
 
@@ -51,18 +58,18 @@ Editor.Panel.extend({
       //   console.log(`get-canvas-children callback : length - ${length}`);
       // });
 
-    });
+    // });
 
-    this.$dump.addEventListener('confirm', () => {
-      Editor.Scene.callSceneScript('ccc-load-points', 'getfirstchildren-points', '', function (err, points) {
-        if (err) {
-          Editor.log(err);
-        }
-        // console.log(`get-canvas-children callback : length - ${length}`);
+    // this.$dump.addEventListener('confirm', () => {
+    //   Editor.Scene.callSceneScript('ccc-load-points', 'getfirstchildren-points', '', function (err, points) {
+    //     if (err) {
+    //       Editor.log(err);
+    //     }
+    //     // console.log(`get-canvas-children callback : length - ${length}`);
 
-        Editor.log(String(points));
-      });
-    });
+    //     Editor.log(String(points));
+    //   });
+    // });
   },
 
   // register your ipc messages here
